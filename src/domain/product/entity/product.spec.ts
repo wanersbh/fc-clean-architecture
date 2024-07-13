bb33ceeb4a536e9ca6db1e93bb0a3c3446eed80f
wanersbh <wanersbh@gmail.com>
2024-07-13 10:54:43 -0300

    feat(desafio): clean arch - notification pattern in products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -13,6 +13,19 @@ describe("Product unit tests", () => {
     }).toThrowError("Name is required");
   });
 
+  it("should throw error when id and name are empty and price is negative", () => {
+    expect(() => {
+      const product = new Product("", "", -1);
+    }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
+  });
+
+  it("should throw error when id and price are empty", () => {
+    expect(() => {
+      const product = new Product("", "",100);
+    }).toThrowError("product: Id is required,product: Name is required");
+  });
+
+
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
